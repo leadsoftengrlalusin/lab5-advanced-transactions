781c24e56e84dd6c34609bf29b3a76d601917c2a
leadsoftengrlalusin <johnrovic.lalusin@gcash.com>
2025-08-05 10:44:02 +0800

    git init

---
+++ src/main/groovy/com/example/advancedtransaction/AdvancedtransactionApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.advancedtransaction
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AdvancedtransactionApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AdvancedtransactionApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/example/advancedtransaction/AdvancedtransactionApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.advancedtransaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdvancedtransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
